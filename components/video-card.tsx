3b3179e6f2de28c3368302ca7ca05f9cc24a148f
haryosumit <haryosumit@gmail.com>
2025-05-07 18:25:29 +0700

    Update video-card.tsx

--- components/video-card.tsx
+++ components/video-card.tsx
@@ -18,7 +18,6 @@ const VideoCard = ({ video }: any) => {
                     single_img={video.single_img}
                     splash_img={video.splash_img}
                     title={video.title}
-                    alt={`Bokep ${video.title}`}
                 />
                 </Link>
             </div>
